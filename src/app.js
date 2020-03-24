218b4802cc16e50959c1dcf8bd60bacf79b9517a
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-24 14:08:39 -0700

    Netlify with express 🎉🎉🎉

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@ const express = require('express')
 const serverless = require('serverless-http')
 
 const app = express()
-const router = express.Router()
 
-router.get('/', (req, res) => {
+app.get('/.netlify/functions/app', (req, res) => {
   res.send('It works!')
 })
-
-app.use('/.netlify/functions/app', router)
 
 module.exports = app
 module.exports.handler = serverless(app)
